6b3aa0303ff6d1b19ee0a0a78cb4eba3733b7cbe
Tomi Pakarinen <tomi.pakarinen@resys.io>
2017-08-30 21:05:16 +0300

    pipe

---
+++ vars/build.groovy
@@ -0,0 +1,26 @@
+def call(body) {
+	pipeline {
+    	agent any
+      	stages {
+        	stage('Build 1') {
+        		when { expression { true } }
+          		steps {
+          			echo('1')
+          		}
+          	}
+        	stage('Build 2') {
+        		when { expression { echo('here') } }
+          		steps {
+          			echo('2')
+          		}
+          	}
+        	stage('Build 3') {
+        		when { expression { true } }
+          		steps {
+          			echo('3')
+
+          		}
+          	}
+        }
+	}
+}
